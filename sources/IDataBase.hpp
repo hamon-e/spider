1145bc239845ec200b949dd9b4fd4b03843befab
Patrick Lin <patrick.lin@epitech.eu>
2017-10-01 18:27:52 +0200

    database interface updated

--- sources/IDataBase.hpp
+++ sources/IDataBase.hpp
@@ -5,7 +5,8 @@ public:
     using ptree = boost::property_tree::ptree;
 
     virtual void insert(ptree const &doc) = 0;
-    virtual ptree const &find(ptree const &query) = 0;
+    virtual ptree *findOne(ptree const &query) = 0;
+    virtual std::vector<ptree> find(ptree const &query);
     virtual void update(ptree const &query, ptree const &update) = 0;
     virtual void remove(ptree const &query) = 0;
 };
