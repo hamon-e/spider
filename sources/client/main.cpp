d50367689a76b19e3421727f646ede8fae46ba07
Benoit Hamon <benoit.hamon@epitech.eu>
2017-10-04 19:43:18 +0200

    watch directory

--- sources/client/main.cpp
+++ sources/client/main.cpp
@@ -1,3 +1,104 @@
-int main(int argc, char const *argv[]) {
+//
+// main.cpp for cpp_spider in sources/client
+//
+// Made by Benoit Hamon
+// Login   <benoit.hamon@epitech.eu>
+//
+// Started on  Wed Oct 04 18:03:49 2017 Benoit Hamon
+// Last update Wed Oct 04 19:42:37 2017 Benoit Hamon
+//
+
+#include <iostream>
+#include <regex>
+#include <boost/dll/import.hpp>
+#include <boost/function.hpp>
+#include <boost/range/iterator_range.hpp>
+
+#include "IModule.hpp"
+
+typedef boost::shared_ptr<IModule> (module_t)();
+
+class ModuleManager {
+  public:
+    ModuleManager(std::string const &dirname = ".");
+    ~ModuleManager();
+
+  public:
+    void run();
+
+  private:
+    struct Library {
+      std::string name;
+      boost::shared_ptr<IModule> module;
+    };
+
+  private:
+    void runLibraries();
+    void runLibrary(std::string const &libraryName);
+    std::vector<std::string> readDirectory();
+
+  private:
+    std::string _dirname;
+    std::vector<Library> _libraries;
+    ModuleCommunication _moduleCommunication;
+};
+
+
+ModuleManager::ModuleManager(std::string const &dirname) : _dirname(dirname) {}
+
+ModuleManager::~ModuleManager() {}
+
+std::vector<std::string> ModuleManager::readDirectory() {
+  boost::filesystem::path p(this->_dirname);
+  std::vector<std::string> res;
+  std::regex regex(".+\\.dll");
+
+  if (is_directory(p)) {
+    for (auto &entry : boost::make_iterator_range(boost::filesystem::directory_iterator(p), {})) {
+      std::string tmp(entry.path().string());
+      if (std::regex_match(tmp, regex))
+	res.push_back(tmp);
+    }
+  }
+  return res;
+}
+
+void ModuleManager::runLibrary(std::string const &libraryName) {
+  boost::filesystem::path shared_library_path(libraryName);
+  boost::function<module_t > creator;
+
+  std::cout << libraryName << std::endl;
+  try {
+    creator = boost::dll::import_alias<module_t>(shared_library_path, "create_module");
+    boost::shared_ptr<IModule> plugin = creator();
+    plugin->start(this->_moduleCommunication);
+    this->_libraries.push_back({libraryName, plugin});
+  } catch (std::exception) {
+    this->_libraries.push_back({libraryName, nullptr});
+  }
+}
+
+void ModuleManager::runLibraries() {
+  std::vector<std::string> librariesNames = this->readDirectory();
+
+  for (auto &libraryName : librariesNames) {
+    if (std::find_if(this->_libraries.begin(),
+		     this->_libraries.end(),
+		     [libraryName](Library &lib) {return lib.name == libraryName;}) == this->_libraries.end()) {
+      runLibrary(libraryName);
+    }
+  }
+}
+
+void ModuleManager::run() {
+  while (true) {
+    this->runLibraries();
+    sleep(1);
+  }
+}
+
+int main() {
+  ModuleManager mod("../");
+  mod.run();
   return 0;
 }
