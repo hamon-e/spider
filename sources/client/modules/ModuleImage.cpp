98c4291affefa3cd91bb4f19183054e299bb8c64
Benoit Hamon <benoit.hamon@epitech.eu>
2017-10-06 21:34:14 +0200

    begin to change module

--- sources/client/modules/ModuleImage.cpp
+++ sources/client/modules/ModuleImage.cpp
@@ -5,83 +5,69 @@
 // Login   <benoit.hamon@epitech.eu>
 //
 // Started on  Thu Oct 05 10:32:15 2017 Benoit Hamon
-// Last update Thu Oct 05 11:02:34 2017 Benoit Hamon
+// Last update Fri Oct 06 21:33:45 2017 Benoit Hamon
 //
-
-
-#include "ModuleImage.hpp"
 
 #include <Windows.h>
 #include <gdiplus.h>
-using namespace Gdiplus;
 
-int GetEncoderClsid(const WCHAR* format, CLSID* pClsid)
-{
-    UINT  num = 0;          // number of image encoders
-    UINT  size = 0;         // size of the image encoder array in bytes
+#include "ModuleImage.hpp"
+
+bool GetEncoderClsid(std::string const &format, CLSID* pClsid) {
+    int num = 0;
+    int size = 0;
 
     ImageCodecInfo* pImageCodecInfo = NULL;
 
     GetImageEncodersSize(&num, &size);
-    if(size == 0)
-        return -1;  // Failure
+    if (size == 0)
+        return false;
 
     pImageCodecInfo = (ImageCodecInfo*)(malloc(size));
-    if(pImageCodecInfo == NULL)
-        return -1;  // Failure
+    if (!pImageCodecInfo)
+        return false;
 
     GetImageEncoders(num, size, pImageCodecInfo);
 
-    for(UINT j = 0; j < num; ++j)
-    {
-        if( wcscmp(pImageCodecInfo[j].MimeType, format) == 0 )
-        {
-            *pClsid = pImageCodecInfo[j].Clsid;
-            free(pImageCodecInfo);
-            return j;  // Success
+    for (int j = 0; j < num; ++j) {
+        if (wcscmp(pImageCodecInfo[j].MimeType, format) == 0) {
+	    *pClsid = pImageCodecInfo[j].Clsid;
+            ::free(pImageCodecInfo);
+            return true;
         }
     }
-    free(pImageCodecInfo);
-    return -1;  // Failure
+    ::free(pImageCodecInfo);
+    return false;
 }
 
 int main() {
-
-// get the device context of the screen
     HDC hScreenDC = CreateDC("DISPLAY", NULL, NULL, NULL);
-// and a device context to put it in
     HDC hMemoryDC = CreateCompatibleDC(hScreenDC);
 
     int width = GetDeviceCaps(hScreenDC, HORZRES);
     int height = GetDeviceCaps(hScreenDC, VERTRES);
 
-// maybe worth checking these are positive values
     HBITMAP hBitmap = CreateCompatibleBitmap(hScreenDC, width, height);
-
-// get a new bitmap
     HBITMAP hOldBitmap = (HBITMAP) SelectObject(hMemoryDC, hBitmap);
 
     BitBlt(hMemoryDC, 0, 0, width, height, hScreenDC, 0, 0, SRCCOPY);
     hBitmap = (HBITMAP) SelectObject(hMemoryDC, hOldBitmap);
 
-// clean up
     DeleteDC(hMemoryDC);
     DeleteDC(hScreenDC);
 
+    Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+    ULONG_PTR Gdiplus::gdiplusToken;
+    Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
 
-    GdiplusStartupInput gdiplusStartupInput;
-    ULONG_PTR gdiplusToken;
-    GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-
-    //HBITMAP hBitmap = (HBITMAP)LoadImage(GetModuleHandle(NULL), "babe.bmp", IMAGE_BITMAP, 0,0, LR_LOADFROMFILE);
     Bitmap *image = new Bitmap(hBitmap, NULL);
 
     CLSID myClsId;
-    int retVal = GetEncoderClsid(L"image/bmp", &myClsId);
+    int retVal = GetEncoderClsid("image/bmp", &myClsId);
 
     image->Save(L"output.bmp", &myClsId, NULL);
     delete image;
 
-    GdiplusShutdown(gdiplusToken);
+    Gdiplus::GdiplusShutdown(gdiplusToken);
     return 0;
 }
--- sources/client/modules/ModuleImage.hpp
+++ sources/client/modules/ModuleImage.hpp
@@ -5,8 +5,26 @@
 // Login   <benoit.hamon@epitech.eu>
 //
 // Started on  Thu Oct 05 10:32:29 2017 Benoit Hamon
-// Last update Thu Oct 05 11:02:44 2017 Benoit Hamon
+// Last update Fri Oct 06 21:33:51 2017 Benoit Hamon
 //
 
 #pragma once
 
+#include "../IModule.hpp"
+#include "../IModuleCommunication.hpp"
+
+class ModuleExplorer : public IModule {
+public:
+    ModuleExplorer(IModuleCommunication *);
+
+public:
+    void start() override;
+
+public:
+    static boost::shared_ptr<ModuleExplorer> create(IModuleCommunication *);
+
+private:
+
+private:
+    IModuleCommunication *_moduleCommunication;
+};
