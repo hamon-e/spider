581d50bc21221e232a90aabe240ee7523d1757ee
Patrick Lin <patrick.lin@epitech.eu>
2017-10-08 21:21:16 +0200

    clean server main

--- sources/server/main.cpp
+++ sources/server/main.cpp
@@ -10,30 +10,6 @@
 namespace pt = boost::property_tree;
 
 int main(int argc, char const *argv[]) {
-/*
-    MongoDB db;
-    pt::ptree tree;
-
-    tree.put("id", 1);
-    tree.put("timestamp", 239);
-    tree.put("part", 2);
-    tree.put("checksum", 6);
-    tree.put("data.key", "oiergoerihgerio");
-    tree.put("data.process", "discord");
-
-    db.insert("parts", tree);
-*/
-    MongoDB db;
-    pt::ptree tree;
-    tree.put("id.a", 1);
-    pt::ptree tree2;
-    tree2.put("id.a", 5);
-    db.insert("aze", tree);
-    db.update("aze", tree, tree2, true);
-    tree.put("id.a", 5);
-    db.update("aze", tree, tree2, true);
-    db.remove("aze", tree2);
-
     if (argc != 2) {
         std::cerr << "Usage : " << argv[0] << " port" << std::endl;
         return 1;
@@ -43,10 +19,6 @@ int main(int argc, char const *argv[]) {
         boost::asio::io_service ioService;
         Server server(ioService, std::atoi(argv[1]));
         server.start();
-        // IntervalService it(ioService, 5, [](boost::system::error_code const &ec) {
-        //     std::cout << "azeazeaz" << std::endl;
-        // });
-        // it.start();
         ioService.run();
     } catch (std::exception &err) {
         std::cout << err.what() << std::endl;
