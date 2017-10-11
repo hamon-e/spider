bc41fa1e1ace2ad9b93e99eb11f4574e9c33f834
Nicolas Goudal <ngoudal.pro@gmail.com>
2017-10-11 02:27:53 +0200

    db bug tests

--- sources/server/HttpsRestApi.cpp
+++ sources/server/HttpsRestApi.cpp
@@ -5,7 +5,7 @@
 // Login   <nicolas.goudal@epitech.eu>
 //
 // Started on  Sun Oct 08 18:38:49 2017 Nicolas Goudal
-// Last update Sun Oct 08 22:07:06 2017 Nicolas Goudal
+// Last update Mon Oct 09 00:34:48 2017 Nicolas Goudal
 //
 
 #include "SimpleWebServer/client_https.hpp"
@@ -57,28 +57,58 @@ int main() {
     try {
       pt::ptree iptree;
       pt::read_json(request->content, iptree);
+      pt::ptree optree;
 
-      pt::ptree doc;
       try {
-	ptree query;
-	query.put("cookie", iptree.get<std::string>("cookie"));
+	pt::ptree query;
+	query.put("id", "0");
 	db.findOne("Users", query);
-
-	doc.put("id", g_user_id++);
-	doc.put("user", iptree.get<std::string>("user"));
-	doc.put("password", Crypto::md5(iptree.get<std::string>("password")));
-	db.insert("Users", doc);
+	try {
+	  pt::ptree query;
+	  query.put("cookie", iptree.get<std::string>("cookie"));
+	  db.findOne("Users", query);
+	  try {
+	    pt::ptree doc;
+	    doc.put("id", std::to_string(g_user_id));
+	    doc.put("user", iptree.get<std::string>("user"));
+	    doc.put("password", Crypto::md5(iptree.get<std::string>("password")));
+	    db.insert("Users", doc);
+	    optree.put("status", "ok");
+	    optree.put("message", "New user created");
+	    optree.put("id", std::to_string(g_user_id));
+	    ++g_user_id;
+	  }
+	  catch (const std::exception &e) {
+	    response->write(SimpleWeb::StatusCode::client_error_bad_request, e.what());
+	  }
+	}
+	catch (const std::exception &e) {
+	  optree.put("status", "ko");
+	  optree.put("message", "User not logged in");
+	  optree.put("ErrorCode", e.what());
+	}
       }
       catch (const std::exception &e) {
-	doc.put("id", g_user_id++);
-	doc.put("user", iptree.get<std::string>("user"));
-	doc.put("password", Crypto::md5(iptree.get<std::string>("password")));
-	db.insert("Users", doc);
+	response->write(SimpleWeb::StatusCode::client_error_bad_request, e.what());
+	try {
+	  pt::ptree doc;
+	  doc.put("id", std::to_string(g_user_id));
+	  doc.put("user", iptree.get<std::string>("user"));
+	  doc.put("password", Crypto::md5(iptree.get<std::string>("password")));
+	  pt::write_json(std::cout, iptree);
+	  pt::write_json(std::cout, doc);
+	  pt::write_json(std::cout, doc);
+	  db.insert("Users", doc);
+	  optree.put("status", "ok");
+	  optree.put("message", "New user created");
+	  optree.put("message", e.what());
+	  optree.put("id", std::to_string(g_user_id));
+	  ++g_user_id;
+	}
+	catch (const std::exception &e) {
+	  response->write(SimpleWeb::StatusCode::client_error_bad_request, e.what());
+	}
       }
-
-      pt::ptree optree;
-      optree.put("status", "ok");
-      optree.put("id", g_user_id);
 
       std::stringstream json;
       pt::write_json(json, optree);
@@ -94,24 +124,32 @@ int main() {
     try {
       pt::ptree iptree;
       pt::read_json(request->content, iptree);
-
       pt::ptree optree;
+
       try {
-	ptree query;
+	pt::ptree query;
 	query.put("user", iptree.get<std::string>("user"));
 	query.put("password", Crypto::md5(iptree.get<std::string>("password")));
-	ptree user = db.findOne("Users", query);
-
-	user.put("cookie", GenCookie());
-	db.update("Users", query, user);
-	optree.put("status", "ok");
+	pt::ptree user = db.findOne("Users", query);
+	try {
+	  std::string cookie = GenCookie();
+	  user.put("cookie", cookie);
+	  db.update("Users", query, user);
+	  optree.put("status", "ok");
+	  optree.put("message", "User Connected");
+	  optree.put("user", iptree.get<std::string>("user"));
+	  optree.put("id", user.get<std::string>("id"));
+	  optree.put("cookie", cookie);
+	}
+	catch (const std::exception &e) {
+	  response->write(SimpleWeb::StatusCode::client_error_bad_request, e.what());
+	}
       }
       catch (const std::exception &e) {
 	optree.put("status", "ko");
-	optree.put("exception", e.what());
+	optree.put("message", "Bad crementials");
+	optree.put("ErrorCode", e.what());
       }
-
-      optree.put("id", g_user_id);
 
       std::stringstream json;
       pt::write_json(json, optree);
